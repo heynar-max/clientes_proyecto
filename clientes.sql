8700cac24062849ded93173e445f3c65bba58066
Heynar Soto Holguin <36316522+heynar-max@users.noreply.github.com>
2023-04-18 16:08:43 -0500

    Add files via upload

--- clientes.sql
+++ clientes.sql
@@ -1,4 +1,22 @@
+select *
+from clientes;
+
+-- nombre direecion localidad y telefono de los clientes
+
+select Nombre_completo, Dirección, Localidad_Código_postal, Teléfono
+from clientes;
+
+-- identificacion de los clientes, nombre completo, fecha de creacion y grupo de cliente
+
+select ID, Nombre_completo, Fecha_creacion,Grupo_cliente
+from clientes;
+
+-- desceunto en sus compleaños
+
+select ID, Nombre_completo,Teléfono, Fecha_nacimiento
+from clientes;
+
 -- clientes que mas compran localidad
 
 select Nombre_completo, Localidad_Código_postal
-from clientes;
+from clientes
